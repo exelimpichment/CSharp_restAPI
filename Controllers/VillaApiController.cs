9a50bb6cbaa0d2ef63d757e1467401eaa3a7b02c
exelimpichment <oldestspy@gmail.com>
2023-07-20 02:42:12 +0200

    first controller and model added

---
+++ Controllers/VillaApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaApi")] // "api/[controller]" can be also used 
+    [ApiController] //atribute that notifies the app that this gonna be a controller
+    public class VillaApiController : ControllerBase // derives from ControllerBase class
+    {
+      [HttpGet]
+      public IEnumerable<Villa> GetVillas() // the interface in c#
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1, Name="Poll View"},
+                new Villa{Id=2, Name="Beach View"}
+            };
+        }
+    }
+}
+ 
